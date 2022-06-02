783840ff48d5ae76a0a1793a19c4686d43ef324b
ramin988 <105160420+ramin988@users.noreply.github.com>
2022-06-02 13:53:55 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+function result() {
+    var numberValue = document.getElementById("txtInput").value;
+switch (numberValue) {
+  case "1":
+    console.log("farvardin");
+    break;
+  case "2":
+    console.log("ordibehesht");
+    break;
+  case "3":
+    console.log("khordad");
+    break;
+  case "4":
+    console.log("tir");
+    break;
+  case "5":
+    console.log("mordad");
+    break;
+  case "6":
+    console.log("shahrivar");
+    break;
+  case "7":
+    console.log("mehr");
+    break;
+  case "8":
+    console.log("aban");
+    break;
+  case "9":
+    console.log("azar");
+    break;
+  case "10":
+    console.log("dey");
+    break;
+  case "11":
+    console.log("bahman");
+    break;
+  case "12":
+    console.log("esfand");
+    break;
+  default:
+    console.log("noting in month");
+    break;
+}
+
+  
+}
